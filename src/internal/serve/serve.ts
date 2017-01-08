393cabb70dff4c14579736e36919f2e45eb2c99e
basarat <basaratali@gmail.com>
2017-01-08 13:26:19 +1100

    server

--- src/internal/serve/serve.ts
+++ src/internal/serve/serve.ts
@@ -1,5 +1,11 @@
 import { ServeConfig } from '../../types';
 
 export function serve(config: ServeConfig) {
+  return new Server(config);
+}
 
+export class Server { 
+  constructor(config: ServeConfig) {
+    
+  }
 }
